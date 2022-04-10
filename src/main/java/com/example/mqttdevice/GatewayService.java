fac5c95250431074c84710ed3f2154aab9eb6dfe
Bomou He <paakmau@outlook.com>
2022-04-10 19:03:34 +0800

    上传浴室数据

--- src/main/java/com/example/mqttdevice/GatewayService.java
+++ src/main/java/com/example/mqttdevice/GatewayService.java
@@ -7,14 +7,44 @@ import javax.annotation.PostConstruct;
 
 import com.example.protocon.core.Gateway;
 import com.example.protocon.core.Response;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class GatewayService {
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class UploadMessage {
+        ArrayList<ArrayList<Long>> data;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class SensorData {
+        long ts;
+        int value;
+    }
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
     @Autowired
     Gateway gateway;
+
+    @Autowired
+    MqttBathroomLightGateway lightGateway;
+
+    @Autowired
+    MqttBathroomMotionGateway motionGateway;
 
     @PostConstruct
     void init() {
@@ -25,6 +55,28 @@ public class GatewayService {
         });
 
         gateway.registerRequestHandler((short) 0x0004, (client, request) -> {
+            try {
+                var msg = objectMapper.readValue(request.getData(), UploadMessage.class);
+                for (var item : msg.data) {
+                    switch (item.get(0).intValue()) {
+                        case 5895:
+                            motionGateway.sendToMqtt(
+                                    objectMapper
+                                            .writeValueAsString(new SensorData(item.get(1), item.get(2).intValue())),
+                                    "v1/devices/me/telemetry");
+                            break;
+                        case 7125:
+                            lightGateway.sendToMqtt(
+                                    objectMapper
+                                            .writeValueAsString(new SensorData(item.get(1), item.get(2).intValue())),
+                                    "v1/devices/me/telemetry");
+                            break;
+                    }
+                }
+            } catch (Exception e) {
+                log.error("解析失败, data: {}", request.getData());
+            }
+
             return new Response(Instant.now().getEpochSecond(), (byte) 0x00, "{}");
         });
     }
